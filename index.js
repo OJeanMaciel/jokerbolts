4af319e4fe54ca3234e3347f8096bb2c79884a13
Jean Naciel <jeansud@hotmail.com>
2021-02-17 20:42:55 -0300

    Commit da massa

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var http = require('http');
 
 var port = (process.env.PORT || 5000);
 
+require('./bot.js')();
 
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'application/json'});
