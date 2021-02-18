40a81f3208827256b05feb1703fa1c3b531aecc8
Jean Naciel <jeansud@hotmail.com>
2021-02-17 22:05:26 -0300

    +1

--- bot.js
+++ bot.js
@@ -17,15 +17,4 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
 });
-const bot = new TelegramBot(token, {polling: true});
-// Matches "/echo [whatever]"
-bot.onText(/\/Diga seu nome (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-  const chatId = msg.chat.id;
-  const resp = match["Meu nome é: JokerBot"]; // the captured "whatever"
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
 }
